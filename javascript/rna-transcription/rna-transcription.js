c621de2ba457b1cb944cce72e6a3e7a008aa19f2
CodeJourneys <caporter01@gmail.com>
2017-12-30 15:19:03 -0700

    Refactoring javascript/rna-transcription

--- javascript/rna-transcription/rna-transcription.js
+++ javascript/rna-transcription/rna-transcription.js
@@ -1,31 +1,25 @@
 var DnaTranscriber = function() {};
 
 DnaTranscriber.prototype.toRna = function(input) {
-  returnVal = '';
 
-  for (var i = 0, len = input.length; i < len; i++)
-  {
-    switch(input.charAt(i))
-    {
-      case 'G':
-        returnVal += 'C';
-        break;
-      case 'C':
-        returnVal += 'G';
-        break;
-      case 'T':
-        returnVal += 'A';
-        break;
-      case 'A':
-        returnVal += 'U';
-        break;
-      default:
-        throw new Error('Invalid input');
-        break;
-    }
-  }
-
-  return returnVal;
+  return input.split('')
+    .map(function(item)
+      {
+        switch(item)
+        {
+          case 'G':
+            return 'C';
+          case 'C':
+            return 'G';
+          case 'T':
+            return 'A';
+          case 'A':
+            return 'U';
+          default:
+            throw new Error('Invalid input');
+        }
+      })
+    .join('');
 };
 
 module.exports = DnaTranscriber;
