1dcb775f60eff745d787234596b4ea833530a4a1
CodeJourneys <caporter01@gmail.com>
2018-01-08 16:26:51 -0700

    cs/bob - Adding solution

--- csharp/bob/Bob.cs
+++ csharp/bob/Bob.cs
@@ -1,9 +1,23 @@
 using System;
+using System.Text.RegularExpressions;
 
 public static class Bob
 {
     public static string Response(string statement)
     {
-        throw new NotImplementedException("You need to implement this function.");
+      bool isSilence = !Regex.IsMatch(statement, @"[A-Za-z0-9?]+") || statement == String.Empty;
+      bool isYelling = statement.ToUpper() == statement && Regex.IsMatch(statement, @"[A-Z]+");
+      bool isQuestion = statement.Trim().EndsWith("?");
+
+      if(isSilence)
+        return "Fine. Be that way!";
+      if(isYelling)
+      {
+        if(isQuestion) return "Calm down, I know what I'm doing!";
+        return "Whoa, chill out!";
+      }
+      if(isQuestion)
+        return "Sure.";
+      return "Whatever.";
     }
 }
