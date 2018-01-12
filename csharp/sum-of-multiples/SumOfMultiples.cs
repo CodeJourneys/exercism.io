b9bef41cca73df1d0c5bb8699539e03bd252c6e6
CodeJourneys <caporter01@gmail.com>
2018-01-12 11:57:03 -0700

    cs/sum-of-multiples - Adding solution

--- csharp/sum-of-multiples/SumOfMultiples.cs
+++ csharp/sum-of-multiples/SumOfMultiples.cs
@@ -1,10 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 public static class SumOfMultiples
 {
     public static int Sum(IEnumerable<int> multiples, int max)
     {
-        throw new NotImplementedException("You need to implement this function.");
+      return multiples
+        .SelectMany(x => Enumerable.Range(1, max / x), (x, i) => x * i)
+        .Where(x => x < max)
+        .Distinct()
+        .Sum();
     }
 }
