f05b119eb90329fdc3f375f506a9df8f35cfe6ff
CodeJourneys <caporter01@gmail.com>
2018-01-16 15:11:22 -0700

    cs/raindrops ~ Adding solution

--- csharp/raindrops/Raindrops.cs
+++ csharp/raindrops/Raindrops.cs
@@ -4,6 +4,9 @@ public static class Raindrops
 {
     public static string Convert(int number)
     {
-        throw new NotImplementedException("You need to implement this function.");
+      string result = number % 3 == 0 ? "Pling" : "";
+      if(number % 5 == 0) result += "Plang";
+      if(number % 7 == 0) result += "Plong";
+      return result == "" ? number.ToString() : result;
     }
 }
