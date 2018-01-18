a1769b59481627de5550823951e3da0291cd11b5
CodeJourneys <caporter01@gmail.com>
2018-01-18 16:51:35 -0700

    ts/rna-transcription ~ Adding solution

--- typescript/rna-transcription/rna-transcription.ts
+++ typescript/rna-transcription/rna-transcription.ts
@@ -1,7 +1,19 @@
-class Transcriptor {
-    toRna( /* Parameters go here */ ) {
-        // Your code here
+export default class Transcriptor {
+    toRna( dna: string ): string {
+        return dna.toUpperCase().split('')
+          .map((c) => {
+            switch (c) {
+              case 'G':
+                return 'C';
+              case 'C':
+                return 'G';
+              case 'T':
+                return 'A';
+              case 'A':
+                return 'U';
+              default:
+                throw new Error("Invalid input DNA.");
+            }
+          }).join('');
     }
 }
-
-export default Transcriptor
