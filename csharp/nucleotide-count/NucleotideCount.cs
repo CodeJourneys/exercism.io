6b5d4c1de6dd2e0c722b58c0e8efd148a23472cd
CodeJourneys <caporter01@gmail.com>
2018-01-18 08:04:11 -0700

    cs/nucleotide-count ~ Adding solution

--- csharp/nucleotide-count/NucleotideCount.cs
+++ csharp/nucleotide-count/NucleotideCount.cs
@@ -1,19 +1,22 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 public class NucleotideCount
 {
     public NucleotideCount(string sequence)
     {
+      NucleotideCounts = new Dictionary<char, int> {
+        ['A'] = sequence.Count(c => c == 'A'),
+        ['C'] = sequence.Count(c => c == 'C'),
+        ['G'] = sequence.Count(c => c == 'G'),
+        ['T'] = sequence.Count(c => c == 'T')
+      };
+      if(NucleotideCounts.Values.Sum() != sequence.Length) throw new InvalidNucleotideException();
     }
 
     public IDictionary<char, int> NucleotideCounts
-    {
-        get
-        {
-            throw new NotImplementedException("You need to implement this function.");
-        }
-    }
+    { get; private set; }
 }
 
 public class InvalidNucleotideException : Exception { }
