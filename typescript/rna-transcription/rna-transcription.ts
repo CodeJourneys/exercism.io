e90b1d9431f0147b902bc76ed3d6154b772ed917
CodeJourneys <caporter01@gmail.com>
2018-01-20 16:53:42 -0700

    ts/rna-transcription ~ Refactoring

--- typescript/rna-transcription/rna-transcription.ts
+++ typescript/rna-transcription/rna-transcription.ts
@@ -1,19 +1,12 @@
 export default class Transcriptor {
     toRna( dna: string ): string {
+        const dnaToRna: { [key: string]: string} = {
+          G: 'C', C: 'G', T: 'A', A: 'U'
+        }
         return dna.toUpperCase().split('')
           .map((c) => {
-            switch (c) {
-              case 'G':
-                return 'C';
-              case 'C':
-                return 'G';
-              case 'T':
-                return 'A';
-              case 'A':
-                return 'U';
-              default:
-                throw new Error("Invalid input DNA.");
-            }
+            if (dnaToRna[c] === undefined) throw new Error("Invalid input DNA.");
+            return dnaToRna[c];
           }).join('');
     }
 }
