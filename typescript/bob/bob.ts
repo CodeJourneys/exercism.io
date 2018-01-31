820b5af738d43712c39a093c67901f93edf45eb8
CodeJourneys <caporter01@gmail.com>
2018-01-31 12:30:52 -0700

    ts/bob ~ Adding solution

--- typescript/bob/bob.ts
+++ typescript/bob/bob.ts
@@ -1,7 +1,15 @@
-class Bob {
-    hey( /* Parameters go here */ ) {
-        // Your code here
+export default class Bob {
+    hey( str: string ): string {
+      str = str.trim()
+      const isYelling = /[A-Z]+/.test(str) && str.toUpperCase() === str,
+            isQuestion = str.substr(str.length - 1) === "?",
+            isSilence = str === ''
+      if (isYelling) {
+        if (isQuestion) return "Calm down, I know what I'm doing!"
+        return "Whoa, chill out!"
+      }
+      if (isQuestion) return "Sure."
+      if (isSilence) return "Fine. Be that way!"
+        return "Whatever."
     }
 }
-
-export default Bob
